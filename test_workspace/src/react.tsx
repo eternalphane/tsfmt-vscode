6dee9d4c4106d8a6799d837af70006edd79ac7d2
EternalPhane <eternalphane@gmail.com>
2019-01-04 22:41:44 +0800

    update test

---
+++ test_workspace/src/react.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+const logo = require('./logo.svg');
+
+const ugly = () => {
+    return (
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.tsx</code> and save to reload.
+          </p>
+        </div>
+    );
+};
